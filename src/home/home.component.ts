9ef82aea6dbf7475e6fc559ac7840e8b6ce133ce
Miguel Padilla <0619889697@uma.es>
2025-03-18 17:25:34 +0100

    [mariabonafonte] Added navbar prototype

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  private onScroll = () => {
+    console.log('Scroll detected'); // Mensaje de prueba
+    const menu = document.getElementById('menu') as HTMLElement | null;
+    if (menu) {
+      if (window.scrollY > 100) {
+        menu.style.height = '100vh';
+        menu.style.visibility = 'visible';
+      } else {
+        menu.style.height = '0';
+        menu.style.visibility = 'hidden';
+      }
+    }
+  };
+
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.onScroll);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.onScroll);
+  }
 }
