d7657ec466f497c6914bda54b653e8391309c808
Miguel Padilla <0619889697@uma.es>
2025-04-05 17:58:48 +0200

    Added team members info + Code cleanup + Added comments + Animation hotfix

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -9,90 +9,125 @@ import { Component, OnInit, OnDestroy, HostListener, signal, WritableSignal } fr
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
-  isDiscoverVisible: WritableSignal<boolean> = signal(true);
-
-  private animationFinished = false;
-
-  currentYear: number = new Date().getFullYear();
-
-  public onScroll = () => {
-    console.log('Scroll detected'); 
-    const menu = document.getElementById('menu') as HTMLElement | null;
-    if (menu) {
-      this.updateNavbarVisibility(undefined, window.scrollY); // Usar la nueva función
-      if (window.scrollY > 100) {
-        // visibilidad controlada por UpdateNavbar
-      } else {
-        this.isDiscoverVisible.set(false);
-        console.log('isDiscoverVisible', this.isDiscoverVisible()); 
-      }
+  public teamMembers = [
+    {
+      photo: './assets/img/Foto_Nina-Souto-Blanco.jpg',
+      name: 'Nina Souto Blanco',
+      title: 'Co-founder',
+      description: `Biochemistry student at Universidad de Málaga. Passioned for biomedicine and the study of human diseases, I focus my formation in the use of bioinformatic tools to impulse research, optimize clinical diagnosis and contribute to improve the health and quality of life of people.`,
+    },
+    {
+      photo: './assets/img/Foto_Miguel-Leonardo-Padilla-Romo.jpeg',
+      name: 'Miguel Padilla',
+      title: 'Co-founder',
+      description: `Health Engineering student at Universidad de Málaga, specialized in bioinformatics and interested in web development. I began my career developing a customized genomic data exploration tool to support researchers in their work. I am passionate about helping scientists achieve meaningful breakthroughs.`,
+    },
+    {
+      photo: './assets/img/Foto_Juan-Antonio-de-la-Cruz-Bellido.jpeg',
+      name: 'Juan Antonio de la Cruz Bellido',
+      title: 'Founder & CEO',
+      description: `Biology student at Universidad de Málaga, with great interest for and some experience in synthetic biology, bioinformatics (within the genomic and protein engineering fields) as well as bioentrepreneurship, focused on team leadership and achieving advancements on the research world.`,
+    },
+    {
+      photo: './assets/img/Foto_Sofia-Zafirova.jpg',
+      name: 'Sofía Zafirova',
+      title: 'Co-founder',
+      description: `Biology student at universidad de Málaga, passioned about the field of diagnosis and research of the human body. My main interest is focused on making known and explaining the fundamental use of modern-day bioinformatics in facilitating the work of people belonging to the scientific sector.`,
+    },
+    {
+      photo: './assets/img/default_user.png',
+      name: 'Juan Diego López',
+      title: 'BioFounder',
+      description: `Biochemistry student at University de Málaga. I feel a special inclination towards bioinformatics, specially focused on analyzing biological data of any kind, and applied to automated classification systems and virtual data analysis. All centered around impulsing biology research, biochemstry and biotechnology.`,
+    },
+    {
+      photo: './assets/img/default_user.png',
+      name: 'Isabel Fernández',
+      title: 'BioFounder',
+      description: `Biology student at Univerdad de Málaga. I am passionateabout genomic and protein analuysis to deepen our understanding of dieeases and develop new biomedical tools. I would like to apply biological data processing to improve all health-related research.`,
+    },
+    {
+      photo: './assets/img/Foto-Maria_Bonafonte.jpeg',
+      name: 'María Bonafante',
+      title: 'BioFounder',
+      description: `Health Engineering student at the University of Málaga. I love programming and its application in the healthcare field, with a special interest in optimizing processes to improve medical care. I enjoy collaborating on innovative projects, convinced that teamwork is key to contributing to people’s well-being and health.`,
+    },
+    {
+      photo: './assets/img/default_user.png',
+      name: 'Miembro 8',
+      title: 'Co-founder',
+      description: `Biology student at universidad de Málaga, passioned about the field of diagnosis and research of the human body. My main interest is focused on making known and explaining the fundamental use of modern-day bioinformatics in facilitating the work of people belonging to the scientific sector.`,
+    },
+    {
+      photo: './assets/img/default_user.png',
+      name: 'Miembro 9',
+      title: 'Co-founder',
+      description: `Biology student at universidad de Málaga, passioned about the field of diagnosis and research of the human body. My main interest is focused on making known and explaining the fundamental use of modern-day bioinformatics in facilitating the work of people belonging to the scientific sector.`,
+    },
+    {
+      photo: './assets/img/default_user.png',
+      name: 'Miembro 10',
+      title: 'Co-founder',
+      description: `Biology student at universidad de Málaga, passioned about the field of diagnosis and research of the human body. My main interest is focused on making known and explaining the fundamental use of modern-day bioinformatics in facilitating the work of people belonging to the scientific sector.`,
     }
-  };
-
+  ];
+  /**
+   * Is discover section visible flag
+   */
+  isDiscoverVisible: WritableSignal<boolean> = signal(true);
+  /**
+   * Current year for footer
+   */
+  currentYear: number = new Date().getFullYear();
+  /**
+   * Toggles vibility on discover section
+   */
   private toggleDiscoverSection = () => {
     const discoverSection = document.getElementById('discover') as HTMLElement | null;
     if (discoverSection) {
       if (window.scrollY > 100 && discoverSection.style.height !== '0px') {
+        this.isDiscoverVisible.set(true);
         discoverSection.style.height = '0px';
+        document.body.style.pointerEvents = 'none';
+        document.body.style.overflow = 'hidden';
+        discoverSection.style.transform = "translateY(-14rem)";
+        setTimeout(() => {
+          document.body.style.pointerEvents = 'auto';
+          document.body.style.overflow = '';
+        }, 1100);
       } else if (window.scrollY <= 100 && discoverSection.style.height !== 'auto') {
+        this.isDiscoverVisible.set(true);
         discoverSection.style.height = '100vh';
+        discoverSection.style.transform = '';
       }
     }
   }
 
   ngOnInit(): void {
-    // Simular el fin de la animación después de 1 segundo
-    setTimeout(() => {
-    this.animationFinished = true;
-    console.log('Animation finished');
-    this.updateNavbarVisibility(); // después de la animación
-    }, 500); // Ajusta este tiempo según la duración de tu animación (1s en el CSS)  
-
-    window.addEventListener('scroll', this.onScroll);
     window.addEventListener('scroll', this.toggleDiscoverSection);
+    window.addEventListener('mousemove', this.toggleNavbarVisibility);
   }
 
   ngOnDestroy(): void {
-    window.removeEventListener('scroll', this.onScroll);
     window.removeEventListener('scroll', this.toggleDiscoverSection);
+    window.removeEventListener('mousemove', this.toggleNavbarVisibility);
   }
-
-  @HostListener('window:mousemove', ['$event'])
-  onMouseMove(event: MouseEvent): void {
-    const menu = document.getElementById('menu');
-    console.log("event.clientY", event.clientY);
-    this.updateNavbarVisibility(event.clientY); // Usar la nueva función
-  }
-
-  @HostListener('window:scroll')
-  makeDiscoverSectionVisible() {
-    if (window.scrollY < 100) {
-      this.isDiscoverVisible.set(true);
-      const hideDiscover = document.getElementById('hide-discover-btn') as HTMLInputElement | null;
-      if (hideDiscover) {
-        hideDiscover.checked = true; // Check the checkbox if scrolling back up
+  
+  /**
+   * Toggles navbar visibility when moving mouse to top edge
+   * @param event to get current mouse position
+   */
+  private toggleNavbarVisibility(event: MouseEvent): void {
+    const navBar = document.getElementById('navbar');
+    const discoverSection = document.getElementById('discover') as HTMLElement | null;
+    if (navBar) {
+      if (event.clientY <= 100 && discoverSection?.style.height === '0px') {
+        navBar.style.height = '100px';
+        navBar.style.visibility = 'visible';
+      } else {
+        navBar.style.height = '0';
+        navBar.style.visibility = 'hidden';
       }
-    }
-  }
-
-  // Nueva función para combinar las condiciones de ratón y scroll
-  private updateNavbarVisibility(clientY?: number, scrollY: number = window.scrollY): void {
-    const menu = document.getElementById('menu') as HTMLElement | null;
-    if (!menu) return;
-
-    if (!this.animationFinished) {
-      menu.style.height = '0';
-      menu.style.visibility = 'hidden';
-      return;
-    }
-
-    const isMouseAtTop = clientY !== undefined && clientY <= 100;
-    if (isMouseAtTop) {
-      menu.style.height = '100px';
-      menu.style.visibility = 'visible';
-    } else {
-      menu.style.height = '0';
-      menu.style.visibility = 'hidden';
     }
   }
 }
